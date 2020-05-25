2503c201d989b01f642030cfdb58f5e0adc1f2cc
Nidhin P <nidhin.p3@gmail.com>
2020-05-26 00:48:55 +0530

    txt file reader

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { memo, useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = memo(function App(props) {
+  const [values, setValues] = useState({ delimiter: '|', lines: 1 });
+  const [errors, setErrors] = useState({});
+  const [fileData, setFileData] = useState([]);
+
+  useEffect(() => {
+    let dropArea = document.getElementsByTagName("BODY")[0];;
+    dropArea.addEventListener('dragenter', onDrag, false)
+    dropArea.addEventListener('dragover', onDrag, false)
+    dropArea.addEventListener('drop', onDrop, false)
+    return () => {
+      dropArea.removeEventListener('dragenter', onDrag);
+      dropArea.removeEventListener('dragover', onDrag);
+      dropArea.removeEventListener('drop', onDrop);
+    }
+  }, []);
+
+  const onDrag = (e) => {
+    e.preventDefault();
+  }
+  const onDrop = (e) => {
+    e.preventDefault()
+    let dt = e.dataTransfer
+    let files = dt.files;
+    console.log("files", files)
+    readTextFile(files)
+  }
+  const readTextFile = (files) => {
+    if (files.length > 0 && files[0].type === "text/plain") {
+      const file = files[0];
+      setErrorValues("file", '')
+      const reader = new FileReader()
+      reader.onload = async (e) => {
+        let text = e.target.result
+        text = text.split(/\r?\n/);
+        setFileData(text);
+      }
+      reader.readAsText(file)
+    } else {
+      setErrorValues('file', 'Invalid file');
+    }
+  };
+
+  const onFileSelect = (e) => {
+    let files = e.target.files;
+    readTextFile(files)
+  }
+
+  const setErrorValues = (name, value) => {
+    setErrors((errs) => ({ ...errs, [name]: value }))
+  }
+
+  const onChangeText = (e) => {
+    let { name, value } = e.target;
+    setValues(v => ({ ...v, [name]: value }))
+  }
+
+  const getTableData = () => {
+    let newData = [...fileData];
+    newData.length = values.lines || 0;
+    return <table>
+      <tbody>
+        {newData.map((item, r) => (
+          <tr key={r} className="tr">
+            {item.split(values.delimiter).map((v, i) => (
+              <td key={i}>{v}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="root">
+      <div className="form-field">
+
+        <label htmlFor="myfile">Select a file:</label>
+        <input type="file" accept=".txt" htmlFor="myfile" onChange={onFileSelect} />
+      </div>
+      <div className="error">{errors.file}</div>
+      <div className="form-field">
+        <div className="field">
+          <label htmlFor="delimiter">Delimiter:</label>
+          <input type="text" name="delimiter" onChange={onChangeText} value={values.delimiter} />
+        </div>
+        <div className="field">
+          <label htmlFor="lines">Lines:</label>
+          <input type="number" name="lines" onChange={onChangeText} value={values.lines} />
+        </div>
+      </div>
+      <div>
+        {getTableData()}
+      </div>
     </div>
   );
-}
+})
 
 export default App;
